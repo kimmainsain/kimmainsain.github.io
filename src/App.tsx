700100a35217f2b6ee671912e958f13d1c4ec04f
kimmainsain <kmsid003@gmail.com>
2023-09-10 18:47:08 +0900

    Setting : tailwind 설정 및 테스트 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          href="https://reactjs.org"
-          className="App-link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="space-y-4">
+      <div className="w-96 bg-red-100 shadow rounded">w-96</div>
+      <div className="w-80 bg-blue-400 shadow rounded">w-80</div>
+      <div className="w-72 bg-green-500 shadow rounded">w-72</div>
+      <div className="w-64 bg-yellow-100 shadow rounded">w-64</div>
+      <div className="w-60 bg-indigo-200 shadow rounded">w-60</div>
+      <div className="w-56 bg-purple-300 shadow rounded">w-56</div>
+      <div className="w-52 bg-pink-400 shadow rounded">w-52</div>
+      <div className="w-48 bg-gray-500 shadow rounded">w-48</div>
     </div>
   );
 }
