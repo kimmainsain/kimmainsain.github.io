24e1cd094b776cbedabbe2a39f4128c679d7f9b8
kimmainsain <kmsid003@gmail.com>
2023-10-13 00:58:46 +0900

    Feat : Main화면 초기 구상

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="w-3/5 h-2/5 bg-blue-500 justify-center items-center flex ">
+      contact
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/pages/History.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const History = () => {
+  return (
+    <div className="w-2/5 h-3/5 bg-red-500 justify-center items-center flex ">
+      history
+    </div>
+  );
+};
+
+export default History;
