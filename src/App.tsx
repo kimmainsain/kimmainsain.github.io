648aed4af5f3a3e5f29fe10dca27fa458174372b
kimmainsain <kmsid003@gmail.com>
2023-10-13 23:24:30 +0900

    Feat : marquee 생성, hover적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from "react";
-// import Contact from "./pages/Contact";
-// import History from "./pages/History";
-// import Project from "./pages/Project";
-// import Strength from "./pages/Strength";
-// import UserInfo from "./pages/UserInfo";
+
 const App = () => {
   return (
-    <div className="w-full h-screen relative">
-      <div className="border-solid border-2 border-black justify-center items-center flex absolute top-0 left-0 w-3/5 h-2/5 bg-blue-500">
-        우아아
+    <div className="w-full h-screen relative m-0 p-0 box-border">
+      <div className="z-80 w-full h-12 bg-white border-2 border-solid border-black justify-start items-center flex absolute top-0 overflow-hidden">
+        <div className="marquee__inner">위</div>
       </div>
-      <div className="border-solid border-2 border-black justify-center items-center flex absolute top-0 right-0 w-2/5 h-3/5 bg-red-500">
-        우아아
+
+      <div className="z-80 w-full h-12 bg-white border-2 border-solid border-black justify-start items-center flex absolute bottom-0 overflow-hidden">
+        <div className="marquee__inner">아래</div>
       </div>
-      <div className="border-solid border-2 border-black justify-center items-center flex absolute bottom-0 left-0 w-2/5 h-3/5 bg-green-500">
-        우아아
+
+      <div className="z-90 w-12 h-screen bg-white border-2 border-solid border-black justify-center items-start flex absolute right-0 overflow-hidden">
+        <div className="marquee__inner">오른쪽</div>
       </div>
-      <div className="border-solid border-2 border-black justify-center items-center flex absolute bottom-0 right-0 w-3/5 h-2/5 bg-yellow-500">
-        우아아
+
+      <div className="z-90 w-12 h-screen bg-white border-2 border-solid border-black justify-center items-start flex absolute left-0 overflow-hidden">
+        <div className="marquee__inner">왼쪽</div>
       </div>
-      <div className="border-solid border-2 border-black justify-center items-center flex absolute top-2/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/5 h-1/5 bg-purple-500">
-        우아아
+
+      <div className="group m-12 absolute top-0 bottom-0 left-0 right-0">
+        <div className="hover:text-red-500 transition duration-500 hover:scale-125 z-10 w-3/5 h-2/5 bg-white border-2 border-black justify-center items-center flex absolute top-0 left-0">
+          Contact
+        </div>
+        <div className="hover:text-blue-500 transition duration-500 hover:scale-125 z-10 w-2/5 h-3/5 bg-white border-2 border-black justify-center items-center flex absolute top-0 right-0">
+          History
+        </div>
+        <div className="hover:text-gray-500 transition duration-500 hover:scale-125 z-10 w-2/5 h-3/5 bg-white border-2 border-black justify-center items-center flex absolute bottom-0 left-0">
+          Project
+        </div>
+        <div className="hover:text-sky-500 transition duration-500 hover:scale-125 z-10 w-3/5 h-2/5 bg-white border-2 border-black justify-center items-center flex absolute bottom-0 right-0">
+          Strength
+        </div>
+        <div className="hover:text-yellow-500 transition duration-500 hover:scale-125 z-10 w-1/5 h-1/5 bg-purple-500 border-2 border-black justify-center items-center flex absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          UserInfo
+        </div>
       </div>
     </div>
   );
