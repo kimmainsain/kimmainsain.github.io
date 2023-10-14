3954d26d6b021028d7059f4b522d74911b6585fb
kimmainsain <kmsid003@gmail.com>
2023-10-15 00:17:11 +0900

    Feat : contact 확장이벤트 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/mainLayout.scss";
 
 const App = () => {
+  const [hoveredBox, setHoveredBox] = useState(null as string | null);
+
   return (
     <div className="container">
       <div className="header marquee">위</div>
-
       <div className="footer marquee bottom">아래</div>
-
-      <div className="right-sidebar marquee right">오른</div>
-
-      <div className="left-sidebar marquee left">왼</div>
-
-      <div>
-        <div className="contact inner-box">Contact</div>
-        <div className="history inner-box">History</div>
-        <div className="project inner-box">Project</div>
-        <div className="strength inner-box">Strength</div>
-        <div className="user-info inner-box">UserInfo</div>
+      <div className="main-wrapper">
+        <div
+          className={`contact inner-box ${
+            hoveredBox === "contact" ? "expanded" : ""
+          }`}
+          onMouseEnter={() => setHoveredBox("contact")}
+          onMouseLeave={() => setHoveredBox(null)}
+        >
+          Contact
+        </div>
+        <div
+          className={`history inner-box ${
+            hoveredBox === "history" ? "expanded" : ""
+          }`}
+          onMouseEnter={() => setHoveredBox("history")}
+          onMouseLeave={() => setHoveredBox(null)}
+        >
+          History
+        </div>
+        <div
+          className={`project inner-box ${
+            hoveredBox === "project" ? "expanded" : ""
+          }`}
+          onMouseEnter={() => setHoveredBox("project")}
+          onMouseLeave={() => setHoveredBox(null)}
+        >
+          Project
+        </div>
+        <div
+          className={`strength inner-box ${
+            hoveredBox === "strength" ? "expanded" : ""
+          }`}
+          onMouseEnter={() => setHoveredBox("strength")}
+          onMouseLeave={() => setHoveredBox(null)}
+        >
+          Strength
+        </div>
+        <div
+          className={`user-info inner-box ${
+            hoveredBox === "user-info" ? "expanded" : ""
+          }`}
+          onMouseEnter={() => setHoveredBox("user-info")}
+          onMouseLeave={() => setHoveredBox(null)}
+        >
+          UserInfo
+        </div>
       </div>
     </div>
   );
