dbf47bf51f143da8747f7a77b7cea534d63ccd72
kimmainsain <kmsid003@gmail.com>
2023-10-14 23:25:45 +0900

    Feat : marquee 복구, marquee left, right삭제

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,13 @@ import "./styles/mainLayout.scss";
 const App = () => {
   return (
     <div className="container">
-      <div className="header">
-        <div>위</div>
-      </div>
+      <div className="header marquee">위</div>
 
-      <div className="footer">
-        <div>아래</div>
-      </div>
+      <div className="footer marquee bottom">아래</div>
 
-      <div className="right-sidebar sidebar">
-        <div>오른쪽</div>
-      </div>
+      <div className="right-sidebar marquee right">오른</div>
 
-      <div className="left-sidebar sidebar">
-        <div>왼쪽</div>
-      </div>
+      <div className="left-sidebar marquee left">왼</div>
 
       <div>
         <div className="contact inner-box">Contact</div>
