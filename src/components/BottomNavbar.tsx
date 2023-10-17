76578f0349dd6471b62cc8d7f0053ab5a1159df1
kimmainsain <kmsid003@gmail.com>
2023-10-18 01:22:58 +0900

    Feat : bottomNav 현재 location hoverEffect 적용

--- src/components/BottomNavbar.tsx
+++ src/components/BottomNavbar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import "../styles/bottomNav.scss";
 import logo from "../assets/images/mainlogo.png";
 
 const BottomNavbar = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleNavigation = (path: string) => {
     navigate(path);
@@ -12,23 +13,41 @@ const BottomNavbar = () => {
 
   return (
     <nav role="navigation" className="nav-menu">
-      <div onClick={() => handleNavigation("/about")} className="nav-link">
+      <div
+        onClick={() => handleNavigation("/about")}
+        className={`nav-link ${location.pathname === "/about" ? "active" : ""}`}
+      >
         ABOUT
       </div>
-      <div onClick={() => handleNavigation("/project")} className="nav-link">
+      <div
+        onClick={() => handleNavigation("/project")}
+        className={`nav-link ${
+          location.pathname === "/project" ? "active" : ""
+        }`}
+      >
         PROJECT
       </div>
       <div
         onClick={() => handleNavigation("/")}
-        className="nav-home is--brand"
+        className={`nav-home is--brand ${
+          location.pathname === "/" ? "active" : ""
+        }`}
         aria-label="home"
       >
         <img src={logo} alt="logo" className="logo" />
       </div>
-      <div onClick={() => handleNavigation("/stack")} className="nav-link">
-        TECT STACK
+      <div
+        onClick={() => handleNavigation("/stack")}
+        className={`nav-link ${location.pathname === "/stack" ? "active" : ""}`}
+      >
+        TECH STACK
       </div>
-      <div onClick={() => handleNavigation("/history")} className="nav-link">
+      <div
+        onClick={() => handleNavigation("/history")}
+        className={`nav-link ${
+          location.pathname === "/history" ? "active" : ""
+        }`}
+      >
         HISTORY
       </div>
     </nav>
