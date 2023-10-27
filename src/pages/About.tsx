a0e6f5d448b5414aac8ab163820d89c2a9b3b0c6
kimmainsain <kmsid003@gmail.com>
2023-10-28 01:25:10 +0900

    :heavy_plus_sign: Feat(About) : keyword template 완성

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,6 +3,7 @@ import BottomNavbar from "../components/BottomNavbar";
 import "../styles/aboutLayout.scss";
 import Header from "../components/Header";
 import profilePhoto from "../assets/images/ssunguri.png";
+import "../assets/fonts/fonts.scss";
 
 const About = () => {
   return (
@@ -10,7 +11,6 @@ const About = () => {
       <Header mainheader="About ." subheader="개발자 김민섭입니다." />
       <div className="about-card-box">
         <div className="about-card-contents">
-          {/* <div className="about-card-img">이미지</div> */}
           <img src={profilePhoto} alt="profile" className="about-card-img" />
           <div className="about-card-explain">
             <div className="about-card-explain-head">나를 표현하는 한줄</div>
@@ -18,6 +18,29 @@ const About = () => {
               두세줄정도 적을 공간 + 깃허브 주소 + 블로그 링크
             </div>
           </div>
+        </div>
+      </div>
+      <div className="about-keywords">
+        <div className="keyword-item">
+          <div className="keyword-head">#키워드 A</div>
+          <div className="keyword-body">
+            질문, 토론 등 소통을 활성화해 참여도를 높입니다.
+          </div>
+          <img src={profilePhoto} alt="profile" className="keyword-img" />
+        </div>
+        <div className="keyword-item">
+          <div className="keyword-head">#키워드 B</div>
+          <div className="keyword-body">
+            질문, 토론 등 소통을 활성화해 참여도를 높입니다.
+          </div>
+          <img src={profilePhoto} alt="profile" className="keyword-img" />
+        </div>
+        <div className="keyword-item">
+          <div className="keyword-head">#키워드 C</div>
+          <div className="keyword-body">
+            질문, 토론 등 소통을 활성화해 참여도를 높입니다.
+          </div>
+          <img src={profilePhoto} alt="profile" className="keyword-img" />
         </div>
       </div>
       <BottomNavbar />
