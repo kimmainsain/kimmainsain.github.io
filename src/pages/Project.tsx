3d71d5688a3630d2957aca6f29153a76b755a301
kimmainsain <kmsid003@gmail.com>
2023-10-28 18:29:26 +0900

    :heavy_plus_sign: Feat(Project) : slider, carousel setting 완료

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,11 +1,39 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../styles/projectLayout.scss";
 import BottomNavbar from "../components/BottomNavbar";
 import Header from "../components/Header";
 
+const sliderSettings = {
+  dots: true,
+  infinite: true,
+  speed: 700,
+  centerMode: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: true,
+};
+
+const slideData = ["Slide 1 Content", "Slide 2 Content", "Slide 3 Content"];
+
 const Project = () => {
   return (
-    <div>
+    <div className="project-page">
       <Header mainheader="Project ." subheader="FE / AOS" />
+      <div className="project-container">
+        <Slider {...sliderSettings}>
+          {slideData.map((slide, index) => (
+            <div key={index} className="slide-item">
+              {slide}
+            </div>
+          ))}
+        </Slider>
+      </div>
+
       <BottomNavbar />
     </div>
   );
